100cd767fa2d349368d606a954ead52d56a721c0
Diego Queiroz dos Santos <diego.freebsd@gmail.com>
2018-10-21 19:55:57 -0300

    .

--- scripts/imprime_ticket.sh
+++ scripts/imprime_ticket.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cp scripts/impressao.zpl scripts/tmp.zpl
+cd scripts
+cp impressao.zpl tmp.zpl
 
 SUBJECT=3a
 SEARCH_FOR=%1
 REPLACE_WITH=$1
 
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR/$REPLACE_WITH/g" scripts/tmp.zpl > scripts/tmp.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR/$REPLACE_WITH/g" tmp.zpl > tmp.zpl
 
-lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw scripts/tmp1.zpl
+lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw tmp1.zpl
