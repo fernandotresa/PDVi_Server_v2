8edf66e228843d7cd64b1a119333362800c509c1
Diego Queiroz dos Santos <diego.freebsd@gmail.com>
2018-10-21 19:58:14 -0300

    .

--- scripts/imprime_ticket.sh
+++ scripts/imprime_ticket.sh
@@ -9,4 +9,4 @@ REPLACE_WITH=$1
 
 echo "$SUBJECT" | sed -e "s/$SEARCH_FOR/$REPLACE_WITH/g" tmp.zpl > tmp.zpl
 
-lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw tmp1.zpl
+lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw tmp.zpl
