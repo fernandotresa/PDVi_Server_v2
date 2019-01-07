fbaf1fd09f9eb4666bacffb6bf771d05bda13f9c
Diego Queiroz dos Santos <admin@dbltecnologia.com.br>
2019-01-07 18:08:18 -0200

    .

--- scripts/impressao.sh
+++ scripts/impressao.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
 cd scripts
+rm tmp*.zpl
 cp impressao.zpl tmp.zpl
 
 SUBJECT=3a
-SEARCH_FOR=%1
-REPLACE_WITH=$1
 
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR/$REPLACE_WITH/g" tmp.zpl > tmp1.zpl
+SEARCH_FOR_TICKET_TYPE=%1
+REPLACE_WITH_TICKET_TYPE=$1
 
-lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw tmp1.zpl
+SEARCH_FOR_VALUE=%2
+REPLACE_WITH_VALUE=$2
+
+SEARCH_FOR_OP=%3
+REPLACE_WITH_OP=$3
+
+SEARCH_FOR_DATE=%4
+REPLACE_WITH_DATE=$4
+
+SEARCH_FOR_TICKET=%5
+REPLACE_WITH_TICKET=$5
+
+SEARCH_FOR_TOTAL=%6
+REPLACE_WITH_TOTAL=$6
+
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TICKET_TYPE/$REPLACE_WITH_TICKET_TYPE/g" tmp.zpl > tmp1.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_VALUE/$REPLACE_WITH_VALUE/g" tmp1.zpl > tmp2.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_OP/$REPLACE_WITH_OP/g" tmp2.zpl > tmp3.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_DATE/$REPLACE_WITH_DATE/g" tmp3.zpl > tmp4.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TICKET/$REPLACE_WITH_TICKET/g" tmp4.zpl > tmp5.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TOTAL/$REPLACE_WITH_TOTAL/g" tmp5.zpl > tmp6.zpl
+
+lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw tmp6.zpl
--- scripts/tmp6.zpl
+++ scripts/tmp6.zpl
@@ -1,12 +1,12 @@
 ^XA
 
 ^CFA,15
-^FO250,10^FD%1^FS
-^FO250,30^FDR$%2^FS
+^FO250,10^FDMEIA^FS
+^FO250,30^FDR$20.00^FS
 ^FO250,50^FDOperador^FS
-^FO250,70^FD%3^FS
-^FO250,90^FD%4^FS
-^FO250,110^FDBilhete %5^FS
+^FO250,70^FDARIEL^FS
+^FO250,90^FD01012000^FS
+^FO250,110^FDBilhete 1234567^FS
 
 ^CFA,15
 ^FO450,10^FDIngresso valido para um acesso^FS
@@ -14,11 +14,11 @@
 ^FO400,50^FDFundacao Parque zoologico de Sao Paulo^FS
 ^FO450,70^FDCNPJ: 60.889.573/00001-40^FS
 
-^FO450,90^FDTotal da venda: R$ %6^FS
+^FO450,90^FDTotal da venda: R$ 200.00^FS
 ^FO450,110^FDFotografia: Carlos N.^FS
 ^FO450,130^FDTecnologia: www.3a.com.br^FS
 
 ^BY5,2,70
-^FO250,180^BC^FD%5^FS
+^FO250,180^BC^FD1234567^FS
 
 ^XZ
