11d95fd80f497d2f0687274c143f906020f2f32d
Diego Queiroz dos Santos <admin@dbltecnologia.com.br>
2019-01-08 09:43:06 -0200

    .

--- scripts/impressao.sh
+++ scripts/impressao.sh
@@ -24,11 +24,11 @@ REPLACE_WITH_TICKET=$5
 SEARCH_FOR_TOTAL=%6
 REPLACE_WITH_TOTAL=$6
 
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TICKET_TYPE/$REPLACE_WITH_TICKET_TYPE/g" tmp.zpl > tmp1.zpl
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_VALUE/$REPLACE_WITH_VALUE/g" tmp1.zpl > tmp2.zpl
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_OP/$REPLACE_WITH_OP/g" tmp2.zpl > tmp3.zpl
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_DATE/$REPLACE_WITH_DATE/g" tmp3.zpl > tmp4.zpl
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TICKET/$REPLACE_WITH_TICKET/g" tmp4.zpl > tmp5.zpl
-echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TOTAL/$REPLACE_WITH_TOTAL/g" tmp5.zpl > tmp6.zpl
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TICKET_TYPE/$REPLACE_WITH_TICKET_TYPE/g" tmp.zpl > tmp1.zpl &&
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_VALUE/$REPLACE_WITH_VALUE/g" tmp1.zpl > tmp2.zpl &&
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_OP/$REPLACE_WITH_OP/g" tmp2.zpl > tmp3.zpl &&
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_DATE/$REPLACE_WITH_DATE/g" tmp3.zpl > tmp4.zpl &&
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TICKET/$REPLACE_WITH_TICKET/g" tmp4.zpl > tmp5.zpl &&
+echo "$SUBJECT" | sed -e "s/$SEARCH_FOR_TOTAL/$REPLACE_WITH_TOTAL/g" tmp5.zpl > tmp6.zpl 
 
 lpr -P Zebra_Technologies_ZTC_GC420t_ -o raw tmp6.zpl
