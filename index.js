d2c6701cea01cf00ed40cc50715d22b2a1c393f5
Diego Queiroz dos Santos <admin@dbltecnologia.com.br>
2019-01-16 15:08:40 -0200

    .

--- index.js
+++ index.js
@@ -34,19 +34,19 @@ var db_config_remote = {
     database: "vendas_online"
 };
 
-/*var db_config_local = {
+var db_config_local = {
     host: "10.8.0.50",
     user: "root",
     password: "Mudaragora00",
     database: "zoosp"
-};*/
+};
 
-var db_config_local = {
+/*var db_config_local = {
     host: "10.0.2.180",
     user: "root",
     password: "Mudaragora00",
     database: "zoosp"
-};
+};*/
 
 let con;
 let conLocal;
@@ -576,6 +576,41 @@ function payProductContinue(req, product, data){
     }
 }
 
+function confirmCashDrain(req, res){
+
+    let idUser = req.body.idUser
+    let idSupervisor = req.body.idSupervisor
+    let drainValue = req.body.drainValue
+                
+    let sql = "INSERT INTO 3a_sangria (fk_id_usuario, fk_id_supervisor, data_sangria, valor_sangria) \
+        VALUES (" + idUser + ", " + idSupervisor + ", NOW(), " + drainValue + ")";
+
+    log_(sql)
+
+    conLocal.query(sql, function (err1, result) {        
+        if (err1) throw err1;           
+        res.json({"success": result}); 
+    });
+}
+
+function confirmCashChange(req, res){
+
+    let idUser = req.body.idUser
+    let idSupervisor = req.body.idSupervisor
+    let changeValue = req.body.changeValue
+                
+    let sql = "INSERT INTO 3a_troco (fk_id_usuario, fk_id_supervisor, data_inclusao, valor_inclusao) \
+        VALUES (" + idUser + ", " + idSupervisor + ", NOW(), " + changeValue + ")";
+
+    log_(sql)
+
+    conLocal.query(sql, function (err1, result) {        
+        if (err1) throw err1;           
+        res.json({"success": result}); 
+    });
+
+}
+
 app.post('/getAllOrders', function(req, res) {    
 
     let start = req.body.start
@@ -822,14 +857,10 @@ app.post('/getAuth', function(req, res) {
 });
 
 app.post('/getAuthSupervisor', function(req, res) {    
-    let email = req.body.email
-    let password = req.body.password
                 
     let sql = "SELECT * FROM 3a_usuarios \
         INNER JOIN 3a_nivel_acesso ON  3a_nivel_acesso.id_nivel_acesso = 3a_usuarios.fk_id_nivel_acesso \
-        where login_usuarios = '" + email + "' \
-        AND senha_usuarios_pdvi = '" + password + "' \
-        AND (3a_nivel_acesso.id_nivel_acesso = 2 OR 3a_nivel_acesso.id_nivel_acesso = 3);";
+        where 3a_nivel_acesso.id_nivel_acesso = 2 OR 3a_nivel_acesso.id_nivel_acesso = 3;";
 
     log_(sql)
 
@@ -891,37 +922,11 @@ app.post('/getTicketOperator', function(req, res) {
 });
 
 app.post('/confirmCashDrain', function(req, res) {    
-
-    let idUser = req.body.idUser
-    let idSupervisor = req.body.idSupervisor
-    let drainValue = req.body.drainValue
-                
-    let sql = "INSERT INTO 3a_sangria (fk_id_usuario, fk_id_supervisor, data_sangria, valor_sangria) \
-        VALUES (" + idUser + ", " + idSupervisor + ", NOW(), " + drainValue + ")";
-
-    log_(sql)
-
-    conLocal.query(sql, function (err1, result) {        
-        if (err1) throw err1;           
-        res.json({"success": result}); 
-    });
+    confirmCashDrain(req, res)
 });
 
 app.post('/confirmCashChange', function(req, res) {    
-
-    let idUser = req.body.idUser
-    let idSupervisor = req.body.idSupervisor
-    let changeValue = req.body.changeValue
-                
-    let sql = "INSERT INTO 3a_troco (fk_id_usuario, fk_id_supervisor, data_inclusao, valor_inclusao) \
-        VALUES (" + idUser + ", " + idSupervisor + ", NOW(), " + changeValue + ")";
-
-    log_(sql)
-
-    conLocal.query(sql, function (err1, result) {        
-        if (err1) throw err1;           
-        res.json({"success": result}); 
-    });
+    confirmCashChange(req, res)
 });
 
 app.post('/getCashDrain', function(req, res) {    
