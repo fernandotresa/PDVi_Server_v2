d4457e988ff8012949d3dac674a44fe727505169
Diego Queiroz dos Santos <admin@dbltecnologia.com.br>
2019-01-25 16:40:44 -0200

    .

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ var db_config_remote = {
 };
 
 var db_config_local = {
-    //host: "10.8.0.50",
+    host: "10.8.0.50",
     //host: "10.19.31.247",
-    host: "10.0.2.180",
+    //host: "10.0.2.180",
     user: "root",
     password: "Mudaragora00",
-    //database: "zoosp"
-    database: "3access"
+    database: "zoosp"
+    //database: "3access"
 };
 
 let con;
